c65e62195a2ff76cae15c88ba379aea3c4706920
Sergei Mikharevich <mikharevichsn@mail.ru>
2021-01-21 22:22:20 +0300

    стили + модалка

---
+++ modal.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    modalBlock: document.querySelector('[data-modal] .modal'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modalBlock.classList.toggle('animate__backInDown');
+  }
+})();
